7be5a96be2712e6bfd53add778d98181da07fb67
Victor Zaichikov <v.zaichikov@gmail.com>
2022-02-04 16:33:56 +0200

    Try to make namespaced

---
+++ namespaced/Barcode/BarcodeValidation.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace BarcodeValidation;
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'barcode-validator.php';
+
+class Barcode_Validator extends \BarcodeValidator {}
