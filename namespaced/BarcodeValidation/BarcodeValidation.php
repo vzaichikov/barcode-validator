73c847ddf489b5c30fe507c9db9c556464078447
Victor Zaichikov <v.zaichikov@gmail.com>
2022-02-04 16:53:48 +0200

    Removed dir

--- namespaced/BarcodeValidation/BarcodeValidation.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace BarcodeValidation;
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'barcode-validator.php';
-
-class Barcode_Validator extends \BarcodeValidator {}
